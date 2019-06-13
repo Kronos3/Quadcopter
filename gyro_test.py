0bd122460e4f1328fabe2a68b36cccb6106b8d2c
Andrei Tumbar <andreitumbar@gmail.com>
2019-06-13 16:15:25 -0400

    count herz, set range to 8G

--- gyro_test.py
+++ gyro_test.py
@@ -1,9 +1,22 @@
 from gyro import Gyro
 import sys
+import time
 
 test = Gyro(0x68)
+test.set_accel_range(Gyro.ACCEL_RANGE_8G)
+
 index = int(sys.argv[1])
 
+start_time = time.time()
+i = 0
+
 while True:
-	data = test.pid_read()
-	sys.stdout.write("\r%s" % data[index])
+	try:
+		i += 1
+		data = test.pid_read()
+		sys.stdout.write("\r%.4f" % data[index])
+	except KeyboardInterrupt:
+		break
+
+total_time = time.time() - start_time
+print("Hz = %s" % (i / total_time))
